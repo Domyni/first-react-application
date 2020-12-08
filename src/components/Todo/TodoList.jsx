e217de32b1b9e204da6db008a3ee18644b8b5bd1
iAmWasin <52755109+Domyni@users.noreply.github.com>
2020-12-08 22:33:43 +0800

    React 04 - List rendering
    Removed array index used as key.
    Replace with UUID

--- src/components/Todo/TodoList.jsx
+++ src/components/Todo/TodoList.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 function todoList (props){
     const taskListItems = props.list;
     return (
         <ul>
-            {taskListItems.map((value, index) => {
-                return <li key={index}> {value} </li>
+            {taskListItems.map((value) => {
+                return <li key = {uuidv4()}> {value} </li>
             })} 
         </ul>
     )
 }
 
-export default todoList
+export default todoList;
